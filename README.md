f811d76080d59422b6031777b14a2d7a14b98a58
Dmitriy Danilin <danilindn@icloud.com>
2023-06-30 18:22:51 +0300

    Добавил код

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ColorWheel
 
-A description of this package.
+Цветовой круг, с помощью которого можно выбрать нужный цвет жестом, и присвоить его любому элементу.
+
+Как использовать
+```Swift
+import ColorWheel
+
+let colorWheel = ColorWheel() // Это обычная view, поэтому можно задать размеры любым способом
+var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(selectColor)
+
+@objc private func selectColor(_ gestureRecognizer: UIPanGestureRecognizer) {
+    let location = gestureRecognizer.location(in: colorView)
+    let color = colorView.colorAtPoint(point: location)
+    view.backgroundColor = color
+}
+```
--- Sources/ColorWheel/ColorWheel.swift
+++ Sources/ColorWheel/ColorWheel.swift
@@ -1,6 +1,82 @@
-public struct ColorWheel {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+private struct ColorPath {
+    var path: UIBezierPath
+    var color: UIColor
+}
+
+public final class ColorWheel: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        center = self.center
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+        center = self.center
+    }
+    
+    private var image: UIImage?
+    private var imageView: UIImageView?
+    private var paths = [ColorPath]()
+    
+    private var size: CGSize = CGSize.zero { didSet { setNeedsDisplay() } }
+    private var sectors: Int = 360 { didSet { setNeedsDisplay() } }
+    
+    public func colorAtPoint(point: CGPoint) -> UIColor {
+        for colorPath in 0..<paths.count {
+            if paths[colorPath].path.contains(point) {
+                return paths[colorPath].color
+            }
+        }
+        return UIColor.clear
+    }
+
+    public override func draw(_ rect: CGRect) {
+        let radius = CGFloat(min(bounds.size.width, bounds.size.height) / 2.0 ) * 0.90
+        let angle: CGFloat = CGFloat(2.0) * (.pi) / CGFloat(sectors)
+        var colorPath: ColorPath = ColorPath(path: UIBezierPath(), color: UIColor.clear)
+        
+        self.center = CGPoint(x: self.bounds.width - (self.bounds.width / 2.0), y: self.bounds.height - (self.bounds.height / 2.0))
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: bounds.size.width, height: bounds.size.height), true, 0)
+        
+        UIColor.white.setFill()
+        UIRectFill(frame)
+        
+        for sector in 0..<sectors {
+            let center = self.center
+            colorPath.path = UIBezierPath(
+                arcCenter: center,
+                radius: radius,
+                startAngle: CGFloat(sector) * angle,
+                endAngle: (CGFloat(sector) + CGFloat(1)) * angle, clockwise: true
+            )
+            colorPath.path.addLine(to: center)
+            colorPath.path.close()
+            
+            let color = UIColor(
+                hue: CGFloat(sector) / CGFloat(sectors),
+                saturation: CGFloat(1),
+                brightness: CGFloat(1),
+                alpha: CGFloat(1)
+            )
+            color.setFill()
+            color.setStroke()
+            
+            colorPath.path.fill()
+            colorPath.path.stroke()
+            colorPath.color = color
+
+            paths.append(colorPath)
+        }
+        
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        guard image != nil else { return }
+        let imageView = UIImageView(image: image)
+        self.addSubview(imageView)
+        guard let oc = superview?.center else { return }
+        self.center = oc
     }
 }
